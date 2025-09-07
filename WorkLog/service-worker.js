e06d18353cb61553654215902cb2057387f077c7
Cosmin19961 <vilca.cosmin@gmail.com>
2025-09-07 15:40:40 +0200

    Delete WorkLog directory

--- WorkLog/service-worker.js
+++
@@ -1,7 +0,0 @@
-self.addEventListener('install', event => {
-  console.log('Service worker installato');
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(fetch(event.request));
-});
